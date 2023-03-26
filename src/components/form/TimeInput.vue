b6f440fa9dd64118af45d832343545055920019d
Miguel Guerrero <miguel_angel_g@live.com>
2023-03-26 19:01:35 -0400

    Simplified the timeinput component. Autoselects the seconds input when the minute input length is exceeded.

--- src/components/form/TimeInput.vue
+++ src/components/form/TimeInput.vue
@@ -1,26 +1,80 @@
 <script setup>
+const placeholder = '00';
+
+function autoFocusNextInputOnMaxLength(event) {
+  const input = event.target;
+  if (input.value.length === 2) {
+    input.blur();
+    document.getElementById('seconds').querySelector('input').focus();
+  }
+}
+
+function clearInput(event) {
+  const input = event.target;
+  input.value = '';
+}
+
 </script>
 
 <template>
   <div class="time-input">
-    <label for="time-input"></label>
-    <input type="text" id="time-input" name="time" pattern="[0-5][0-9]:[0-5][0-9]" required>
+    <span id="minutes" class="time-part">
+      <input name="minutes"
+             min="0"
+             max="59"
+             maxlength="2"
+             :placeholder="placeholder"
+              @focus="clearInput"
+             @input="autoFocusNextInputOnMaxLength"
+      />
+      <label class="small-text" for="minutes">m</label>
+    </span>
+    <div class="delimiter">:</div>
+    <span id="seconds" class="time-part">
+      <input
+          name="seconds"
+          min="0"
+          max="59"
+          maxlength="2"
+          :placeholder="placeholder"
+          @focus="clearInput"
+      />
+      <label class="small-text" for="seconds">s</label>
+    </span>
   </div>
 </template>
 
 <style scoped>
+
 .time-input {
   display: flex;
-  place-items: center;
-  padding: 0 0.2rem;
+  border: var(--color-tertiary) solid 1px;
+  width: fit-content;
+  padding: 0 0.5rem;
 }
 
-input {
-  min-height: 1rem;
-  min-width: 1rem;
+.time-part {
+  display: flex;
+}
+
+.time-part input {
+  border: none;
+  max-width: 2ch;
+  background-color: transparent;
+  padding: 0;
   margin: 0;
-  border-style: none;
-  background: none;
+  font-size: 1rem;
+}
+
+.delimiter {
+  padding: 0 0.5rem;
+}
+
+label {
+  align-self: end;
+  padding: 0;
+  margin: 0;
+  transform: translateY(-1px);
 }
 
 </style>
