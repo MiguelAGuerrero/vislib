58d483b483cd6f6d7e7cd23b693e41df2fb76554
Miguel Guerrero <miguel_angel_g@live.com>
2023-03-26 21:11:59 -0400

    Autofocusing next input only if the input is clean.

--- src/components/form/TimeInput.vue
+++ src/components/form/TimeInput.vue
@@ -13,31 +13,53 @@ defineProps({
   },
 });
 
+function sanitizeInput(input) {
+  const sanitized = input.replace(/\D/g, '');
+  return sanitized;
+}
+
 const emit = defineEmits(['update:modelValue']);
 
-function onBlur() {
-  const minutesBlurred = minutesInput.value !== document.activeElement;
-  const secondsBlurred = secondsInput.value !== document.activeElement;
-  if (minutesBlurred && secondsBlurred) {
-    const minutes = minutesInput.value.value;
-    const seconds = secondsInput.value.value;
-    const SEC_IN_MIN = 60;
-    const totalSeconds = Number(minutes) * SEC_IN_MIN + Number(seconds);
-    emit('update:modelValue', totalSeconds);
+function updateModelValue() {
+  const minutes = sanitizeInput(minutesInput.value.value);
+  const seconds = sanitizeInput(secondsInput.value.value);
+  const SEC_IN_MIN = 60;
+  const totalSeconds = Number(minutes) * SEC_IN_MIN + Number(seconds);
+  emit('update:modelValue', totalSeconds);
+}
+
+function correctInput(event) {
+  const input = event.target;
+  const sanitized = sanitizeInput(input.value);
+  input.value = sanitized;
+}
+
+function updateModelOnCompleteBlur() {
+  const isMinutesInputBlurred = minutesInput.value !== document.activeElement;
+  const isSecondsInputBlurred = secondsInput.value !== document.activeElement;
+  if (isMinutesInputBlurred && isSecondsInputBlurred) {
+    updateModelValue();
   }
 }
 
+function onBlur(event) {
+  correctInput(event);
+  updateModelOnCompleteBlur();
+}
+
 function autoFocusNextInputOnMaxLength(currentInput, nextInput, maxLength) {
-  if (currentInput.value.length === maxLength) {
-    currentInput.blur();
-    nextInput.focus();
-  }
+  if (currentInput.value.length !== maxLength) return;
+  currentInput.blur();
+  nextInput.focus();
 }
 
 function handleMinuteInput(event) {
   const input = event.target;
-  const MAX_INPUT_LENGTH = 2;
-  autoFocusNextInputOnMaxLength(input, secondsInput.value, MAX_INPUT_LENGTH);
+  const sanitized = sanitizeInput(input.value);
+  if (sanitized === input.value) {
+    const MAX_INPUT_LENGTH = 2;
+    autoFocusNextInputOnMaxLength(input, secondsInput.value, MAX_INPUT_LENGTH);
+  }
 }
 
 function clearInput(event) {
