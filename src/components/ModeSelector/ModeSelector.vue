1e4ed692cbd8cc596acea1f2d4c421b69063a2c3
Miguel Guerrero <miguel_angel_g@live.com>
2023-03-27 23:12:50 -0400

    Made ModeSelector have the card template instead.

--- src/components/ModeSelector/ModeSelector.vue
+++ src/components/ModeSelector/ModeSelector.vue
@@ -1,25 +1,31 @@
+<template>
+  <div class="mode-selection even-grow">
+    <div class="mode-card"
+         :class="{ 'selected': selectedMode === mode }"
+         v-for="mode in modes"
+         :key="mode.name"
+         @click="handleCardClick(mode)">
+      <div class="mode-card-icon">
+        <font-awesome-icon :icon="mode.icon"/>
+      </div>
+      <h3 class="mode-card-title">{{ mode.name }}</h3>
+      <p class="mode-card-description">{{ mode.description }}</p>
+      <div class="mode-card-check">
+        <font-awesome-icon :icon="faCheckCircle"
+                           :style='{visibility: selectedMode !== mode ? "hidden" : "visible"}'/>
+      </div>
+    </div>
+  </div>
+</template>
+
 <script setup>
-import { faStopwatch, faBookOpen, faGears } from '@fortawesome/free-solid-svg-icons';
-import ModeCard from './ModeCard.vue';
+import { ref } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+  faStopwatch, faBookOpen, faGears, faCheckCircle,
+} from '@fortawesome/free-solid-svg-icons';
 
-const emit = defineEmits(['update:modelValue']);
-
-// model prop is a string that is the name of the mode
-defineProps({
-  modelValue: {
-    type: String,
-    default: 'Study',
-  },
-});
-
-// function that emits a model update event with the name of the mode selected
-function updateModel(mode) {
-  console.log('update model', mode);
-  emit('update:modelValue', mode);
-}
-
-// A list of objects with a title and description for each mode card
-const modes = [
+const modes = ref([
   {
     name: 'Sketch',
     description: 'Quick 1-minute intervals. Good for practicing gestures drawings or quick sketches',
@@ -35,27 +41,59 @@ const modes = [
     description: 'Define your own settings',
     icon: faGears,
   },
-];
+]);
 
+const selectedMode = ref(null);
+
+const handleCardClick = (mode) => {
+  selectedMode.value = mode;
+};
 </script>
 
-<template>
-  <h2 class="prompt">Choose a mode</h2>
-  <div class="flex small-gap even-grow">
-    <mode-card
-        v-for="mode in modes"
-        :key="mode.name"
-        :icon="mode.icon"
-        :title="mode.name"
-        @select="updateModel(mode.name.toLowerCase())"
-    >
-      {{ mode.description }}
-    </mode-card>
-  </div>
-</template>
-
 <style scoped>
-.prompt {
+
+.mode-selection {
+  display: flex;
+  justify-content: center;
+  align-items: stretch;
   color: var(--color-tertiary);
+  gap: 2rem;
 }
+
+.mode-card {
+  display: flex;
+  flex-direction: column;
+  padding: 1rem;
+  border: 0.063rem solid #ccc;
+  border-radius: 0.25rem;
+  cursor: pointer;
+  transition: box-shadow 0.3s ease-in-out;
+  aspect-ratio: 3/4;
+}
+
+.mode-card.selected {
+  box-shadow: 0 0 2px 2px var(--color-accent);
+}
+
+.mode-card:hover {
+  box-shadow: 0 0 2px 2px var(--color-tertiary);
+}
+
+.mode-card-icon {
+  font-size: 5rem;
+}
+
+.mode-card-title {
+  font-weight: bold;
+}
+
+.mode-card-description {
+  flex-grow: 1;
+}
+
+.mode-card-check {
+  text-align: center;
+  font-size: 2rem;
+}
+
 </style>
