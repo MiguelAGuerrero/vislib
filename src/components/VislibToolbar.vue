c6f4a40b4c77629d100ce9c28a13d89285154087
Miguel Guerrero <miguel_angel_g@live.com>
2023-11-03 07:16:56 -0400

    feat(toolbar): moved the actions to a toolbar

---
+++ src/components/VislibToolbar.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {faClipboard, faPlay, faSave, faUpload} from '@fortawesome/free-solid-svg-icons';
+import VislibToolbarItem from './VislibToolbarItem.vue';
+const emit = defineEmits(['start', 'paste', 'upload', 'save']);
+</script>
+
+<template>
+  <div role="toolbar" class="toolbar">
+    <VislibToolbarItem :icon="faSave" @click="emit('save')">Save Session</VislibToolbarItem>
+    <VislibToolbarItem :icon="faClipboard" @click="emit('paste')">Paste Images</VislibToolbarItem>
+    <VislibToolbarItem :icon="faUpload" @click="emit('upload')">Upload Images</VislibToolbarItem>
+    <VislibToolbarItem :icon="faPlay" @click="emit('start')">Start Session</VislibToolbarItem>
+  </div>
+</template>
+
+<style scoped>
+
+.toolbar {
+  display: flex;
+  flex-direction: row;
+  justify-content: end;
+  font-weight: bold;
+  padding: 0.5rem;
+  gap: 0.5rem;
+}
+
+.toolbar > * {
+  background-color: transparent;
+  color: var(--color-accent);
+}
+
+.toolbar > *:nth-last-child(1) {
+  color : var(--color-primary);
+  background-color: var(--color-accent);
+}
+
+</style>
---
+++ src/components/VislibToolbarItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <button class="action-button" @click="handleClick">
+    <FontAwesomeIcon class="action-button__icon" :icon="icon" v-if="icon" />
+    <slot>
+      <span>{{ text }}</span>
+    </slot>
+  </button>
+</template>
+
+<script setup>
+import { defineEmits } from 'vue';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Define props with type and default values
+defineProps({
+  icon: Object,
+  text: String
+});
+
+const emits = defineEmits(['click']);
+
+// You can add a ref to handle internal states if necessary
+// const internalState = ref(null);
+
+const handleClick = () => {
+  // Emit a custom event when the button is clicked
+  // You can pass relevant data as a payload if needed
+  emits('click');
+};
+</script>
+
+<style>
+.action-button {
+  display: grid;
+  grid-auto-flow: column;
+  place-content: center;
+  gap: 1rem;
+}
+
+.action-button__icon {
+  margin: auto 0;
+  max-height: 1.5rem;
+}
+
+</style>
